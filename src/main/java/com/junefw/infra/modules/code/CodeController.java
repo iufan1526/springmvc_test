848a7c3b21e64ce8deee819d886a7b9f59ed632c
kim <iufan1526@naver.com>
2022-03-02 14:44:25 +0900

    1

---
+++ src/main/java/com/junefw/infra/modules/code/CodeController.java
@@ -0,0 +1,30 @@
+package com.junefw.infra.modules.code;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class CodeController {
+
+	@Autowired
+	CodeServiceImpl service;
+
+	@RequestMapping(value = "/code/codeGroupList")
+	public String CodeGroupList(Model model) throws Exception {
+		
+		List<Code> list = service.selectList();
+		
+		
+		model.addAttribute("list", list);
+		
+		return "code/codeGroupList";
+		
+		
+	}
+	
+}
